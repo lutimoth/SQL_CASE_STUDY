37cb13ade44fb026c057cdd24733d3948de709fc
naturesbless <lutimoth@usc.edu>
2022-06-01 17:39:17 -0700

    Update SQLTasks Tier 2.sql

--- SQLTasks Tier 2.sql
+++ SQLTasks Tier 2.sql
@@ -29,14 +29,20 @@ to your GitHub.
 
 Before starting with the questions, feel free to take your time,
 exploring the data, and getting acquainted with the 3 tables. */
-TEST
 
 /* QUESTIONS 
 /* Q1: Some of the facilities charge a fee to members, but some do not.
 Write a SQL query to produce a list of the names of the facilities that do. */
 
+SELECT name 
+FROM `Facilities` 
+WHERE membercost != 0;
 
 /* Q2: How many facilities do not charge a fee to members? */
+
+SELECT COUNT(*) AS nocharge
+FROM `Facilities` 
+WHERE membercost = 0;
 
 
 /* Q3: Write an SQL query to show a list of facilities that charge a fee to members,
@@ -44,9 +50,16 @@ where the fee is less than 20% of the facility's monthly maintenance cost.
 Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
 
+SELECT facid, name, membercost, monthlymaintenance
+FROM Facilities
+WHERE membercost <= monthlymaintenance * 0.2;
 
 /* Q4: Write an SQL query to retrieve the details of facilities with ID 1 and 5.
 Try writing the query without using the OR operator. */
+
+SELECT *
+FROM Facilities
+WHERE facid IN (1,5);
 
 
 /* Q5: Produce a list of facilities, with each labelled as
@@ -54,15 +67,25 @@ Try writing the query without using the OR operator. */
 more than $100. Return the name and monthly maintenance of the facilities
 in question. */
 
+SELECT name, monthlymaintenance,
+CASE WHEN monthlymaintenance > 100 THEN "expensive"
+	ELSE "cheap" 
+END AS cost
+FROM Facilities;
+
 
 /* Q6: You'd like to get the first and last name of the last member(s)
 who signed up. Try not to use the LIMIT clause for your solution. */
 
+SELECT firstname, surname
+FROM Members
+WHERE joindate = (SELECT MAX(joindate) FROM Members);
 
 /* Q7: Produce a list of all members who have used a tennis court.
 Include in your output the name of the court, and the name of the member
 formatted as a single column. Ensure no duplicate data, and order by
 the member name. */
+
 
 
 /* Q8: Produce a list of bookings on the day of 2012-09-14 which
